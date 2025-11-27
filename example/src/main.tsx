4f8402f935f63ad66a73c37721a7f8621fb48c02
Michael Shimeles <michaelwasihun96@gmail.com>
2025-11-26 23:03:14 -0500

    fix: wrong package

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -5,7 +5,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const address = import.meta.env.VITE_CONVEX_URL;
 
